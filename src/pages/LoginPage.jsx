79395322b8295b103139c46772e1b42bc25ebf1a
KayckRossi <kayck.12gabriel@gmail.com>
2024-11-13 17:31:25 -0300

    subindo scss

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Button, Card, Container, Form } from 'react-bootstrap';
-import '../assets/styles/';
+import '../assets/styles/LoginPage.scss';
 
 function LoginPage() {
   return (
